cc7b571b9b1e07d56c203f553382d7e4043ab96e
bolatalbert <bolatalbert@gmail.com>
2022-06-15 12:12:26 +0300

    Задания 2 и 3 выполнены

---
+++ lesson_1_2_and_3.py
@@ -0,0 +1,24 @@
+import numpy as np
+a = np.array([[1, 6],
+              [2, 8],
+              [3, 11],
+              [3, 10],
+              [1, 7]], dtype=float)
+print("Размер/форма a: {}".format(a.shape), "\nРазмерность a: {}".format(a.ndim))
+
+mean_a = a.mean(axis=0, dtype=float)
+print("Среднее значение по столбцам = {}".format(mean_a))
+
+# задание 2
+a_centered = a - mean_a
+print("Массив a минус среднее значение массива a = {}".format(a_centered))
+print("Размер/форма: {}".format(a_centered.shape), "\nРазмерность: {}".format(a_centered.ndim))
+
+# Задание 3
+print("Первый столбец: {}".format(a[0:, 0]))
+print("Второй столбец: {}".format(a[0:, 1]))
+c = a[0:,0].copy()
+b = a[0:,1].copy()
+print("Произведение первого столбца на второй = : {}".format(c*b))
+
+
